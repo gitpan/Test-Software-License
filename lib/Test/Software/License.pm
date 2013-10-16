23eb099e1c7f8799503f083063c8122a77824f72
Kevin Dawson <bowtie@cpan.org>
2013-10-16 21:05:05 +0000

    Import of BOWTIE/Test-Software-License-0.001003 from CPAN.
    gitpan-cpan-distribution: Test-Software-License
    gitpan-cpan-version:      0.001003
    gitpan-cpan-path:         BOWTIE/Test-Software-License-0.001003.tar.gz
    gitpan-cpan-author:       BOWTIE
    gitpan-cpan-maturity:     released

--- lib/Test/Software/License.pm
+++ lib/Test/Software/License.pm
@@ -6,14 +6,15 @@ use strict;
 use Carp;
 
 use version;
-our $VERSION = '0.001001';
+our $VERSION = '0.001003';
 use English qw( -no_match_vars );    # Avoids reg-ex performance penalty
 local $OUTPUT_AUTOFLUSH = 1;
 
 use parent 0.225 qw(Exporter);
 
 use Data::Printer {caller_info => 1, colored => 1,};
-use Software::LicenseUtils 0.103005;
+# use Software::LicenseUtils 0.103005;
+use Test::Software::LicenseUtils;
 use File::Slurp;
 use File::Find::Rule       ();
 use File::Find::Rule::Perl ();
@@ -85,7 +86,7 @@ sub all_software_license_from_perlmodule_ok {
 		$Test->ok($files[0],
 			'found (' . $found_perl_modules . ') perl modules to test');
 
-		guess_license(\@files);
+		_guess_license(\@files);
 
 	}
 	return;
@@ -108,7 +109,7 @@ sub all_software_license_from_perlscript_ok {
 				'found (' . $found_perl_scripts . ') perl script to test in ' . $dir);
 
 
-			guess_license(\@files);
+			_guess_license(\@files);
 
 		}
 	}
@@ -119,14 +120,14 @@ sub all_software_license_from_perlscript_ok {
 #######
 # composed method test for license
 #######
-sub guess_license {
+sub _guess_license {
 	my $files_ref = shift;
 	my $Test      = Test::Builder->new;
 
 	try {
 		foreach my $file (@{$files_ref}) {
 			my $ps_text = read_file($file);
-			my @guesses = Software::LicenseUtils->guess_license_from_pod($ps_text);
+			my @guesses = Test::Software::LicenseUtils->guess_license_from_pod($ps_text);
 
 			if ($#guesses >= 0) {
 				$Test->ok(1, "$file -> @guesses");
@@ -148,7 +149,7 @@ sub all_software_license_from_metayml_ok {
 		try {
 			my $meta_yml = read_file('META.yml');
 			my @guess_yml
-				= Software::LicenseUtils->guess_license_from_meta($meta_yml);
+				= Test::Software::LicenseUtils->guess_license_from_meta($meta_yml);
 			if (@guess_yml) {
 				$Test->ok(1, "META.yml -> @guess_yml");
 				$passed_a_test = TRUE;
@@ -173,7 +174,7 @@ sub all_software_license_from_metajson_ok {
 		try {
 			my $meta_json = read_file('META.json');
 			my @guess_json
-				= Software::LicenseUtils->guess_license_from_meta($meta_json);
+				= Test::Software::LicenseUtils->guess_license_from_meta($meta_json);
 
 			if (@guess_json) {
 				$Test->ok(1, "META.json -> @guess_json");
@@ -202,26 +203,27 @@ __END__
 
 =head1 NAME
 
-Test::Software::License - [One line description of module's purpose here]
+Test::Software::License - just another xt for Software::License
 
 
 =head1 VERSION
 
-This document describes Test::Software::License version 0.001000
+This document describes Test::Software::License version 0.001002
 
 
 =head1 SYNOPSIS
 
 	use Test::More;
 	use Test::Requires {
-		'Test::Software::License' => 0.001000,
+		'Test::Software::License' => 0.00,
 	};
 
 	all_software_license_ok();
 
 	done_testing();
 
-  
+see eg/ for a compleat test files
+
 =head1 DESCRIPTION
 
 this should be treated as beta, as initial release
@@ -233,93 +235,27 @@ this should be treated as beta, as initial release
 
 =item * all_software_license_from_meta_ok
 
+If you just want to test the META files only
+
 =item * all_software_license_from_metajson_ok
 
 =item * all_software_license_from_metayml_ok
 
 =item * all_software_license_from_perlmodule_ok
 
+If you just want to test the contents of lib directories
+
 =item * all_software_license_from_perlscript_ok
+
+If you just want to test  the contents script and bin directories
 
 =item * all_software_license_ok
 
-=item * guess_license
+This is the main method you should use.
 
 =item * import
 
 =back
-
-=head1 INTERFACE 
-
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
-
-
-=head1 DIAGNOSTICS
-
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
-
-=over
-
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
-
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-Test::Software::License requires no configuration files or environment variables.
-
-
-=head1 DEPENDENCIES
-
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
-
-=head1 INCOMPATIBILITIES
-
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-to get the best out of this you need to apply some of the patches in
-Software::License pull requests
 
 =head1 AUTHOR
 
@@ -341,6 +277,8 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
+
+L<Software::License>
 
 =cut
 
